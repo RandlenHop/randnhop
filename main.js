c7238af50c8334ecdee6c9c0c68def39ac8a4b9e
him <him@him.com>
2022-03-23 12:01:14 +0100

    Added more origin

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ if (process.env.NODE_ENV === "production") {
     "aclshippingsinc.com",
     "https://www.aclshippingsinc.com",
     "https://aclshippingsinc.com",
+    "https://aclshippingsinc.com/",
+    "https://www.aclshippingsinc.com/",
   ];
 } else {
   origin = ["http://localhost:3000"];
