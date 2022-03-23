57bcecea3d5e5ee3af4d91f09aeb0ce442d20215
him <him@him.com>
2022-03-23 11:51:02 +0100

    Removed prefix for frontend link

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 let origin;
 if (process.env.NODE_ENV === "production") {
-  origin = ["https://www.aclshippingsinc.com"];
+  origin = ["aclshippingsinc.com"];
 } else {
   origin = ["http://localhost:3000"];
 }
