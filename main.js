ba89983c9fe02727faf6989d1d7c36037f86b9b7
him <him@jim.com>
2022-04-21 20:15:33 +0100

    Effected more frontend link

--- main.js
+++ main.js
@@ -10,7 +10,10 @@ app.use(express.json());
 
 let origin;
 if (process.env.NODE_ENV === "production") {
-  origin = ["https://www.swizzlloyddelivery.com"];
+  origin = [
+    "https://www.swizzlloyddelivery.com",
+    "https://swizzlloyddelivery.com",
+  ];
 } else {
   origin = ["http://localhost:3000"];
 }
