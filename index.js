0c2a0932ae17726b0572cb510be6252a4aeedd29
@iamstillcath <catherine2lucia@gmail.com>
2024-07-22 14:51:03 +0100

    update

--- index.js
+++ index.js
@@ -29,7 +29,28 @@ const corsOptions = {
   origin: origin,
   optionsSuccessStatus: 200,
 };
-app.use(cors(corsOptions));
+
+// Set CORS headers manually
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", origin.includes(req.headers.origin) ? req.headers.origin : origin[0]);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 
 // Connect Database
 connectDB();
