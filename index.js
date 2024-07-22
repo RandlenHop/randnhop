e6e243f26c0ba1f5380966c338503bdfdb440c9f
@iamstillcath <catherine2lucia@gmail.com>
2024-07-22 16:04:33 +0100

    update

--- index.js
+++ index.js
@@ -10,52 +10,40 @@ app.use(express.json());
 
 // Use git push heroku master to deploy
 
-let allowedOrigins;
+let origin;
 if (process.env.NODE_ENV === 'production') {
-  allowedOrigins = ['https://www.swizzlloyddelivery.com'];
+  origin = [
+    '*',
+    'https://www.swizzlloyddelivery.com',
+    // 'http://localhost:3000',
+    // "https://www.swizzlloyddelivery.com/",
+    // "https://swizzlloyddelivery.com",
+    // "www.swizzlloyddelivery.com/",
+    // "swizzlloyddelivery.com",
+  ];
 } else {
-  allowedOrigins = ['http://localhost:3000'];
+  origin = ['http://localhost:3000'];
 }
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  origin: origin,
   optionsSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 
 // Set CORS headers manually
 app.use((req, res, next) => {
-  const origin = req.headers.origin;
-  if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    "Origin,X-Requested-With,Content-Type",
+    "Accept",
+    "Authorization"
   );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    return res.sendStatus(200);
-  }
 
   next();
 });
+
 
 // Connect Database
 connectDB();
@@ -66,10 +54,10 @@ app.use('/api/delivery', deliveryRoutes);
 // Error Middlewares
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
-// Not found URL middleware
+//Not found URL middleware
 app.use(notFound);
 
-// Error handler for the whole app
+//Error handler for the whole app
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 4011;
