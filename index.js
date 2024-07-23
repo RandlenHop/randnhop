08b9dd087744067e9dab4bfc049f7e57f7c1ca96
@iamstillcath <catherine2lucia@gmail.com>
2024-07-23 10:52:28 +0100

    update

--- index.js
+++ index.js
@@ -52,6 +52,15 @@ connectDB();
 app.use('/api/user', userRoutes);
 app.use('/api/delivery', deliveryRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 // Error Middlewares
 const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
