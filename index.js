ed52ed5a13ea877bcac7d1e41a603225786c7940
@iamstillcath <catherine2lucia@gmail.com>
2024-07-23 11:26:53 +0100

    update

--- index.js
+++ index.js
@@ -55,10 +55,14 @@ app.use('/api/delivery', deliveryRoutes);
 
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname + 'acl-client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'acl-client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'acl-client', 'build', 'index.html'), (err) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+    });
   });
 }
 
