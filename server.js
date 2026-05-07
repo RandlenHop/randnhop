2894c60f77899dd5878010fdd1962aff3ea07551
RandlenHop <randnhop@gmail.com>
2026-05-07 09:58:29 +0100

    Split profile and marketplace routes

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(xss());
 
 // Optimized CORS Configuration
-const allowedOrigins = [
+const allowedOrigins = ["*",
   "http://localhost:5173",
   "http://127.0.0.1:5173",
   "https://randles-hopkick-qkpn.vercel.app" 
