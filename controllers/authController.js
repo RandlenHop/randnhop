d463a5e16cadd1e104376b0b55cbd0415cbafec2
RandlenHop <randnhop@gmail.com>
2026-05-26 11:39:50 +0100

    Updated all marketplace routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,13 +7,16 @@ const nodemailer = require('nodemailer');
 
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    service: 'gmail', 
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  family: 4, // 👈 Force IPv4
+});
 
   const mailOptions = {
     from: `randles and hopkick<${process.env.EMAIL_USER}>`,
