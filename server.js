0b5088e40901aac29086f6621f2a58dd1b140afe
RandlenHop <randnhop@gmail.com>
2026-07-10 19:36:12 +0100

    Updated the staff form

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.use(xss());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5173",
+  "https://www.randleandhopkick.com",
 /\.vercel\.app$/];
 
 const corsOptions = {
